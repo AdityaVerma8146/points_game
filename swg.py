3b16973564c11d83d9ef496f7a28182b6571187a
AdityaVerma8146 <adityaverma8146918@gmail.com>
2025-07-23 14:39:57 +0530

    Add files via upload

---
+++ swg.py
@@ -0,0 +1,27 @@
+import random
+computer= random.choice([-1,0,1])
+youstr = input("enter your choice : ")
+youDict = {"s":1,"w":-1,"g":0}
+reverseDict={1:"Snake",-1:"Water",0:"Gun"}
+
+you = youDict[youstr]
+
+print(f"You choose {reverseDict[you]} \n Computer choose {reverseDict[computer]}")
+if(computer == you):
+    print("it's a draw")
+    
+else:
+    if(computer==-1 and you == 1):
+        print("you win!")
+    elif(computer==-1 and you == 0):
+        print("you win!")
+    elif(computer==1 and you == -1):
+        print("you win!")
+    elif(computer==1 and you == 0):
+        print("you win!")
+    elif(computer==0 and you == -1):
+        print("you win!")
+    elif(computer==0 and you == 1):
+        print("you win!")
+    else:
+        print("something went wrong!")
